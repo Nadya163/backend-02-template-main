450ca03bfaca7bfe52cb8875dcae574b2cf06651
Nadya163 <132590146+Nadya163@users.noreply.github.com>
2023-12-09 19:53:19 +0600

    Исправление и добавление 500 ошибки

--- src/index.js
+++ src/index.js
@@ -49,26 +49,17 @@ const server = http.createServer((request, response) => {
       response.setHeader("Content-Type", "text/plain");
       response.end("internal server error");
     }
+  } else if (queryObject.toString() === "") {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.end("Hello, world!");
   } else {
-    response.statusCode = 200;
-    response.setHeader = "Content-Type: text/plain";
-    response.end("Hello, world!");
+      response.statusCode = 500;
+      response.setHeader("Content-Type", "text/plain");
+      response.end("Internal Server Error");
   }
-
-  // else {
-  //     response.statusCode = 500;
-  //     response.setHeader("Content-Type", "text/plain");
-  //     response.end("Internal Server Error");
-  // }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
-
-// Написать обработчик запроса:
-// - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-// - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-// - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-// - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-// - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
